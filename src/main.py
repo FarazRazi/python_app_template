c56085811d7aa34f9c8c1ee35db761771bf22da5
Faraz Razi <72218210+FarazRazi@users.noreply.github.com>
2023-08-29 11:12:29 +0500

    pylint fixes + removed a lib

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
 def add(var1: int, var2: int):
+    """Function to Add"""
     return var1 + var2
 
 
 def sub(var1: int, var2: int):
+    """Function to Substract"""
     return var1 - var2
 
 
 def mul(var1: int, var2: int):
+    """Function to Multiple"""
     return var1 * var2
 
 
 def div(var1: int, var2: int):
+    """Function to Divide"""
     if var2 == 0:
         raise ValueError('Cannot divide by zero')
 
@@ -29,16 +33,16 @@ if __name__ == '__main__':
             choice = int(input('Enter your choice: '))
             if choice == 5:
                 break
-            var1 = int(input('Enter first number: '))
-            var2 = int(input('Enter second number: '))
+            input1 = int(input('Enter first number: '))
+            input2 = int(input('Enter second number: '))
             if choice == 1:
-                print(add(var1, var2))
+                print(add(input1, input2))
             elif choice == 2:
-                print(sub(var1, var2))
+                print(sub(input1, input2))
             elif choice == 3:
-                print(mul(var1, var2))
+                print(mul(input1, input2))
             elif choice == 4:
-                print(div(var1, var2))
+                print(div(input1, input2))
             else:
                 print('Invalid choice')
             print()
